f7f11c66dbd28d1df2541d3c89db2802c0162d10
Kamrin Kennedy <kamrinkennedy@gmail.com>
2020-05-14 18:08:55 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,22 @@
 class Dog 
   
   attr_accessor :name 
+  @@all = []
   
   def initialize(name)
     @name = name
+    save
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def print_all
+    @@all.each {|dog| puts dog}
+  
+  def save
+    @@all << self
   end
   
 end
